d688f6309c8fb1a72688ff2499a58f31d4dfadac
dimkary <43819376+dimkary@users.noreply.github.com>
2021-03-28 16:43:45 +0200

    Debugged withdrawals

--- multiSIg Wallet/multiSig.sol
+++ multiSIg Wallet/multiSig.sol
@@ -3,6 +3,7 @@ pragma solidity 0.7.5;
 contract multiSig {
     // CONTRACT DATA STRUCTURES DEFINITION
     event depositDone(uint amount, address indexed depositedFrom);
+    event withdrawFail(uint amount, uint contractBalance);
     struct TransferObject {
         address destinationAddress;
         uint amount;
@@ -10,13 +11,21 @@ contract multiSig {
     }
     
     // MODIFIERS
-        modifier balanceCheck(uint _amount) {
-            require(_amount > CONTRACT_BALANCE[address(this)], "Insufficient funds");
+        modifier balanceCheck(uint _amount){
+            require(_amount < CONTRACT_BALANCE[CONTRACT_ADDRESS], "Insufficient funds");
+            _;
+        }
+        
+        modifier checkOwners {
+            require(OWNERS[msg.sender] == true, "Owners only are allowed");
             _;
         }
         
     // STORAGE VALUES
-    address[] private OWNERS;
+
+    address payable CONTRACT_ADDRESS;
+    mapping(address => bool) private OWNERS;
+    address [] private OWNERS_LIST;
     uint private REQUIRED_APPROVALS;
     mapping(address => uint) private CONTRACT_BALANCE;
     TransferObject [] PERDING_TRANSFERS;
@@ -27,21 +36,28 @@ contract multiSig {
         require(_owners.length >= 0, "At leas one owner is required");
         require(_approvals >= 0, "At least one approver is required");
         
-        OWNERS= _owners;
-        OWNERS.push(msg.sender);
+        for (uint i=0; i<_owners.length; i++) {
+            OWNERS[_owners[i]] = true;
+        }
+        OWNERS[msg.sender] = true;
         
+        OWNERS_LIST = _owners;
+        OWNERS_LIST.push(msg.sender);
         REQUIRED_APPROVALS = _approvals;
-        CONTRACT_BALANCE[address(this)] = 0;
+        CONTRACT_ADDRESS = payable(address(this)) ;
+        CONTRACT_BALANCE[CONTRACT_ADDRESS] = 0;
+        
+
         
     }
 
-    function withdraw(uint amount, address payable to) private balanceCheck(amount){
-        CONTRACT_BALANCE[address(this)] -= amount;
+    function withdraw (uint amount, address payable to) private balanceCheck(amount){
+        CONTRACT_BALANCE[CONTRACT_ADDRESS] -= amount;
         to.transfer(amount);
     }
     
     function getOwners() public view returns(address[] memory){
-        return OWNERS;
+        return OWNERS_LIST;
     }
     
     function getApprovals() public view returns(uint){
@@ -49,17 +65,17 @@ contract multiSig {
     }
     
     function getContractBalance() public view  returns(uint){
-        return CONTRACT_BALANCE[address(this)];
+        return CONTRACT_BALANCE[CONTRACT_ADDRESS];
     }
     
     function deposit() public payable returns(uint){
-        CONTRACT_BALANCE[address(this)] += msg.value;
+        CONTRACT_BALANCE[CONTRACT_ADDRESS] += msg.value;
         emit depositDone(msg.value, msg.sender);
-        return CONTRACT_BALANCE[address(this)];
+        return CONTRACT_BALANCE[CONTRACT_ADDRESS];
     }
     
-    function createTransferRequest(address payable _destinationAddress, uint amamamount) public balanceCheck(amamamount){
-        
+    function createTransferRequest (address payable _destinationAddress, uint amamamount) public checkOwners balanceCheck(amamamount) payable{
+
         if (REQUIRED_APPROVALS == 1){
             withdraw(amamamount, _destinationAddress);
         }
